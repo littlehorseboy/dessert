7273e2209b0afb662dd77ff57fd6b36260992bbe
littlehorseboy <littlehorseboy@gmail.com>
2018-09-30 23:02:59 +0800

    feat: shop-list footer
    第一頁完成 雖然還沒有手機版
    
    todo: 要開始第二頁
    
    issue shop-list footer

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,5 @@
 import 'normalize-scss/sass/normalize/_import-now.scss';
 import '../source/style.scss';
+import { MDCTextField } from '@material/textfield';
+
+const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
