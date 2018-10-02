2988c509f15b24379c39a6b1af94594a5ed2da73
littlehorseboy <littlehorseboy@gmail.com>
2018-10-02 23:20:04 +0800

    feat: login
    準備進行第四頁 cart
    
    todo: 手機版
    
    issue login

--- src/js/login/login.js
+++ src/js/login/login.js
@@ -2,4 +2,6 @@ import 'normalize-scss/sass/normalize/_import-now.scss';
 import '../../source/login/style.scss';
 import { MDCTextField } from '@material/textfield';
 
-new MDCTextField(document.querySelector('.mdc-text-field'));
+document.querySelectorAll('.mdc-text-field').forEach(el => {
+  new MDCTextField(el);
+});
