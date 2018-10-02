84921ff36e38e0e7100861bf179738e3d96fafe9
horse <littlehorseboy@gmail.com>
2018-10-02 14:12:44 +0800

    feat: login
    第三頁進行中
    
    todo: 手機版
    
    issue login

--- src/js/login/login.js
+++ src/js/login/login.js
@@ -1,5 +1,5 @@
 import 'normalize-scss/sass/normalize/_import-now.scss';
-import '../../source/product/style.scss';
+import '../../source/login/style.scss';
 import { MDCTextField } from '@material/textfield';
 
 new MDCTextField(document.querySelector('.mdc-text-field'));
