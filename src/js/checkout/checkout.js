44d63e156a7a31661b0d84e0d38cd2f5956820dc
littlehorseboy <littlehorseboy@gmail.com>
2018-10-06 21:16:36 +0800

    feat: RWD
    todo:
    
    issue RWD

--- src/js/checkout/checkout.js
+++ src/js/checkout/checkout.js
@@ -13,21 +13,21 @@ checkoutForm__submit.addEventListener('click', () => {
     <label for="">信用卡卡號</label>
     <input placeholder="9012-3456-7890-1234">
   </div>
-  <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
+  <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6 mdc-layout-grid__cell--span-8-tablet">
     <label for="">持卡人姓名</label>
     <input placeholder="王">
   </div>
-  <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6 mdc-layout-grid__cell--align-bottom">
+  <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6 mdc-layout-grid__cell--span-8-tablet mdc-layout-grid__cell--align-bottom">
     <input placeholder="小明">
   </div>
-  <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
+  <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6 mdc-layout-grid__cell--span-8-tablet">
     <label for="">有效期限</label>
     <input placeholder="月">
   </div>
-  <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6 mdc-layout-grid__cell--align-bottom">
+  <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6 mdc-layout-grid__cell--span-8-tablet mdc-layout-grid__cell--align-bottom">
     <input placeholder="年">
   </div>
-  <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
+  <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6 mdc-layout-grid__cell--span-8-tablet">
     <label for="">背面末三碼</label>
     <input placeholder="123">
   </div>
@@ -50,11 +50,11 @@ checkoutForm__submit.addEventListener('click', () => {
   <input placeholder="12345678">
 </div>`;
     tabs_string.mailing_invoice = `
-<div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
+<div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6 mdc-layout-grid__cell--span-8-tablet">
   <label for="">地址</label>
   <input placeholder="高雄市">
 </div>
-<div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6 mdc-layout-grid__cell--align-bottom">
+<div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6 mdc-layout-grid__cell--span-8-tablet mdc-layout-grid__cell--align-bottom">
   <input placeholder="新興區">
 </div>
 <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
@@ -69,10 +69,10 @@ checkoutForm__submit.addEventListener('click', () => {
   <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
     <div class="checkout-form__title">發票</div>
   </div>
-  <div class="checkout-form__tab active mdc-layout-grid__cell mdc-layout-grid__cell--span-6" data-target="electronic_invoice">
+  <div class="checkout-form__tab active mdc-layout-grid__cell mdc-layout-grid__cell--span-6 mdc-layout-grid__cell--span-8-tablet" data-target="electronic_invoice">
     電子發票
   </div>
-  <div class="checkout-form__tab mdc-layout-grid__cell mdc-layout-grid__cell--span-6" data-target="mailing_invoice">
+  <div class="checkout-form__tab mdc-layout-grid__cell mdc-layout-grid__cell--span-6 mdc-layout-grid__cell--span-8-tablet" data-target="mailing_invoice">
     郵寄發票
   </div>
   <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-12">
